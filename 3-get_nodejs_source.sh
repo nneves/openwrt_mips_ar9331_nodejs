55522c50f2bd9e3cdb9ea3e579916053ed11ce4e
Nelson Neves <nelson.s.neves@gmail.com>
2014-08-05 22:14:08 +0000

    set -j1 and change toolchain to toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2

--- 3-get_nodejs_source.sh
+++ 3-get_nodejs_source.sh
@@ -1,6 +1,7 @@
 # Get custom version of node.js with patch to MIPS platfform
-git clone https://github.com/paul99/v8m-rb.git -b dm-dev-mipsbe
+#git clone https://github.com/paul99/v8m-rb.git -b dm-dev-mipsbe
 cd v8m-rb
+make clean
 make dependencies
 
 # Source this to your shell...
@@ -12,7 +13,7 @@ export STAGING_DIR=${BASEDIR}/staging_dir
 
 # Code Sourcery 2012.03-63 release
 # PREFIX=/home/${USER}/builds/AARC1-Toolchain/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-
-PREFIX=${STAGING_DIR}/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-
+PREFIX=${STAGING_DIR}/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-
 
 export CC=${PREFIX}gcc
 export CXX=${PREFIX}g++
@@ -21,11 +22,24 @@ export RANLIB=${PREFIX}ranlib
 export LINK=$CXX
 
 # LIBPATH=/home/${USER}/builds/AARC1-Toolchain/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/lib/
-LIBPATH=${STAGING_DIR}/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/lib/
+LIBPATH=${STAGING_DIR}/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/lib/
 
 #export LDFLAGS=-L${LIBPATH}
 export LDFLAGS='-Wl,-rpath-link '${LIBPATH}
 
 export GYPFLAGS="-Dv8_use_mips_abi_hardfloat=false -Dv8_can_use_fpu_instructions=false"
-(cd ${V8SOURCE}; ./configure --without-snapshot --shared-v8 --shared-v8-includes=${V8SOURCE}/include/ --shared-v8-libpath=${V8SOURCE}/out/mips.release/lib.target --dest-cpu=mips)
-(cd ${V8SOURCE}; make snapshot=off -j4)
+
+# build v8 as a shared library
+make mips.release library=shared snapshot=off -j1
+
+#echo $V8SOURCE
+#echo $BASEDIR
+#echo $STAGING_DIR
+#echo $CC
+#echo $CXX
+#echo $AR
+#echo $RANLIB
+#echo $LINK
+#echo $LIBPATH
+#echo $LDFLAGS
+#echo $GYPFLAGS
