cebfe91b87648f1be1be51cea19dc72c84caeabc
Nelson Neves <nelson.s.neves@gmail.com>
2014-08-07 23:58:22 +0000

    adding cross-compile npm basic script

---
+++ npm_mips_ar9331.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export BASEDIR=${PWD}/openwrtsource
+export STAGING_DIR=${BASEDIR}/staging_dir
+export NODEJSSOURCE=${PWD}/node-v0.10-5-mips
+
+# Cross Toolchain
+PREFIX=${STAGING_DIR}/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-
+
+export CC=${PREFIX}gcc
+export CXX=${PREFIX}g++
+export AR=${PREFIX}ar
+export RANLIB=${PREFIX}ranlib
+export LINK=$CXX
+
+export npm_config_arch=mips
+
+#<path to the node source>
+export npm_config_nodedir=${NODEJSSOURCE}
+
+npm install serialport
+#npm install socket.io
+#npm install socket.io-client
+
+
